92f02482479d17221845ae5195f67fc566a94904
devluizinwxy <luis.f@academico.ifs.edu.br>
2026-03-24 17:41:08 -0300

    validation

--- src/main/java/com/luisdev/order_management_jpa/entities/dtos/UserRequestDTO.java
+++ src/main/java/com/luisdev/order_management_jpa/entities/dtos/UserRequestDTO.java
@@ -1,4 +1,16 @@
 package com.luisdev.order_management_jpa.entities.dtos;
 
-public record  UserRequestDTO(String name, String email, String password,String cpf) {
+
+import jakarta.validation.constraints.Email;
+import org.hibernate.validator.constraints.br.CPF;
+
+public record  UserRequestDTO(
+
+        String name,
+        @Email
+         String email,
+
+        String password,
+        @CPF
+        String cpf) {
 }
